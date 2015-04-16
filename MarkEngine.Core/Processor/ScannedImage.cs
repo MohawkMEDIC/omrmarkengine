6fb6ffde0558c687e19e1f1278b9d9f069611d70
justin_fyfe <justin@fyfesoftware.ca>
2015-04-16 13:52:19 -0400

    Added license headers

--- MarkEngine.Core/Processor/ScannedImage.cs
+++ MarkEngine.Core/Processor/ScannedImage.cs
@@ -1,4 +1,24 @@
-﻿using AForge.Imaging;
+﻿/* 
+ * Optical Mark Recognition 
+ * Copyright 2014-2015 Justin Fyfe
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you 
+ * may not use this file except in compliance with the License. You may 
+ * obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and limitations under 
+ * the License.
+ * 
+ * Author: Justin
+ * Date: 4-16-2015
+ */
+
+using AForge.Imaging;
 using AForge.Imaging.Filters;
 using AForge.Math.Geometry;
 using System;
@@ -171,8 +191,8 @@ namespace OmrMarkEngine.Core.Processor
                 float rightMost = controlPoints.Select(o => o.X).Max(),
                     leftMost = controlPoints.Select(o => o.X).Min();
                 // Organize those that are left/right
-                AForge.Point[] lefties = controlPoints.Where(o => o.X < (rightMost - leftMost) / 2).ToArray(),
-                    righties = controlPoints.Where(o => o.X > (rightMost - leftMost) / 2).ToArray();
+                AForge.Point[] lefties = controlPoints.Where(o => o.X < leftMost + (rightMost - leftMost) / 2).ToArray(),
+                    righties = controlPoints.Where(o => o.X > leftMost + (rightMost - leftMost) / 2).ToArray();
 
                 // HACK:
                 if (lefties[0].Y < lefties[1].Y)
---
+++ OmrScannerApplication/Sync/RestReturn.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIESeeding.Util
+{
+    /// <summary>
+    /// Return class
+    /// </summary>
+    [DataContract]
+    class RestReturn
+    {
+        // Identifier
+        [DataMember(Name = "id")]
+        public Int32 Id { get; set; }
+    }
+}
